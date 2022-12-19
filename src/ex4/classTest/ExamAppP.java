84cf4d191a29bd628a0e1c5235e64abf2dc37b25
GooGooda <whddn35789@gmail.com>
2022-12-19 09:34:34 +0900

    c

--- src/ex4/classTest/ExamAppP.java
+++ src/ex4/classTest/ExamAppP.java
@@ -2,13 +2,13 @@ package ex4.classTest;
 
 public class ExamAppP {
 public static void main(String[] args) {
-	Exam exams = new Exam();
-	Exam[] exam2 = new Exam[3];
-	
-	exams.kor = 2;
-	exam2[0] = new Exam();
-	exam2[0].kor = 2;
-	System.out.println(exam2[0].kor);
-	
+ Exam exams = new Exam();
+ Exam[] exam = new Exam[3];
+ 
+ exam[0] = new Exam();
+ exam[0].kor = 100;
+ 
+ System.out.println(exam[0].kor);
+ 
 }
 }
