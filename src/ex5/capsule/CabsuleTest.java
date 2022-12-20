8e846c97e27f25f799af39f6e00075b789175113
GooGooDa <70945497+GooGooDa@users.noreply.github.com>
2022-12-20 13:11:56 +0900

    c

--- src/ex5/capsule/CabsuleTest.java
+++ src/ex5/capsule/CabsuleTest.java
@@ -1,25 +1,23 @@
 package ex5.capsule;
 
 public class CabsuleTest {
-public static void main(String[] args) {
-	//성적을 입력받기위한 대상
-	//성적 객체를 준비한다.
-	
-	//성적 뉴렉성적 = new 성적();
-	//성적을 입력 받는다.
-	Exam 뉴렉성적 = new Exam();
-	뉴렉성적.init();
-	
-	//뉴렉성적.입력();
-	Exam.intputExam(뉴렉성적);
-	뉴렉성적.input();
+	public static void main(String[] args) {
+		// 성적을 입력받기위한 대상
+		// 성적 객체를 준비한다.
 
-	//성적을 출력한다.
-	//뉴렉성적.출력();
-	Exam.printExam(뉴렉성적);
-	뉴렉성적.print();
-}
+		// 성적 뉴렉성적 = new 성적();
+		// 성적을 입력 받는다.
+		Exam 뉴렉성적 = new Exam();
+		뉴렉성적.init();
 
+		// 뉴렉성적.입력();
+		Exam.intputExam(뉴렉성적);
+		뉴렉성적.input();
 
+		// 성적을 출력한다.
+		// 뉴렉성적.출력();
+		Exam.printExam(뉴렉성적);
+		뉴렉성적.print();
+	}
 
 }
