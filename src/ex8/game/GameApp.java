7a63398fb9541d6291fcce8701937f7fa519deb9
GooGooDa <70945497+GooGooDa@users.noreply.github.com>
2022-12-22 20:39:54 +0900

    c

--- src/ex8/game/GameApp.java
+++ src/ex8/game/GameApp.java
@@ -6,15 +6,9 @@ public class GameApp {
 public static void main(String[] args) {
 	GameFrame frame = new GameFrame(); //frame 객체
 	
-	
-<<<<<<< HEAD
-	
-		
-=======
 	frame.setSize(500,600);
 	frame.setLocation(100,100);
 	frame.setVisible(true);
 
->>>>>>> refs/remotes/origin/main
 }
 }
