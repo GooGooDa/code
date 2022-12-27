0323e82cbc07e43f919f0520e65e204cac304f96
ict05-07 <ict05-07@DESKTOP-QD8HO1B>
2022-12-27 18:20:34 +0900

    c

--- src/ex08/wrapper/WrapperTest.java
+++ src/ex08/wrapper/WrapperTest.java
@@ -1,9 +1,20 @@
 package ex08.wrapper;
 
+import ex4.classTest.Exam;
+
 public class WrapperTest {
 
 	public static void main(String[] args) {
-Object obj = 3;
+		Exam exam = new Exam();
+		String str = new String("Hello");
+		
+		Object[] list = new String[2];
+		
+		list[0] = str;
+		list[1] = exam;
+		Integer d = 3;
+		int x = d;// object에 기본자료형을 담을수 없음
+		//하지만 컴파일러가 auto boxing 해줌
 	}
 
 }
